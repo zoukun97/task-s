d449bdc0e94bb83af334dacbc0dabb5aa38a8efa
Shuzo Nakajima <zo.shu.0414.73@gmail.com>
2022-05-06 23:39:40 +0900

    userごとにtodoできるように実装

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,7 +2,7 @@ class TodosController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @todos = Todo.all
+    @todos = current_user.todos.all
   end
 
   def new
@@ -10,27 +10,27 @@ class TodosController < ApplicationController
   end
 
   def create
-    todo = Todo.new(todo_params)
+    todo = current_user.todos.build(todo_params)
     todo.save!
     redirect_to todos_url, notice: "タスク「#{todo.name}」を登録しました。"
   end
 
   def show
-    @todo = Todo.find(params[:id])
+    @todo = current_user.todos.find(params[:id])
   end
 
   def edit
-    @todo = Todo.find(params[:id])
+    @todo = current_user.todos.find(params[:id])
   end
 
   def update
-    todo = Todo.find(params[:id])
+    todo = current_user.todos.find(params[:id])
     todo.update!(todo_params)
     redirect_to todos_url, notice: "タスク「#{todo.name}」を更新しました。"
   end
 
   def destroy
-    todo = Todo.find(params[:id])
+    todo = current_user.todos.find(params[:id])
     todo.destroy
     redirect_to todos_url, notice: "タスク「#{todo.name}」を削除しました。"
   end
